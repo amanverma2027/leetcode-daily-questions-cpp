7bd48e75c8d8fa60fdc0a37d883fa26c73be44d1
amanverma2027 <anonymous20062004@gmail.com>
2026-09-05 16:33:34 +0530

    Time: 2 ms (71.88%), Space: 10.4 MB (97.06%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string ok;
+        vector<string>store;
+
+        int i = 1, n = path.size();
+
+        while(i < n) {
+            while(path[i]=='/') i++;
+            while(path[i] and path[i] != '/') ok += path[i++];
+            if(ok == ".." and store.size()) store.pop_back();
+            else if(ok.size() and ok != "." and ok != "..")
+            store.push_back(ok);
+                ok.clear();
+        }
+        ok = "/";
+        for(auto str:store) ok += str, ok += "/";
+        if(ok.size() > 1) ok.pop_back();
+
+        return ok;
+        
+    }
+};
