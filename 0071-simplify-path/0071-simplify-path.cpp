3885db63d984a5e5dfecf67ff0929e6e2a07c402
amanverma2027 <anonymous20062004@gmail.com>
2026-09-05 16:39:49 +0530

    Time: 0 ms (100%), Space: 10.5 MB (95.55%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,22 +1,58 @@
 class Solution {
 public:
     string simplifyPath(string path) {
+        // string ok;
+        // vector<string>store;
+
+        // int i = 1, n = path.size();
+
+        // while(i < n) {
+        //     while(path[i]=='/') i++;
+        //     while(path[i] and path[i] != '/') ok += path[i++];
+        //     if(ok == ".." and store.size()) store.pop_back();
+        //     else if(ok.size() and ok != "." and ok != "..")
+        //     store.push_back(ok);
+        //         ok.clear();
+        // }
+        // ok = "/";
+        // for(auto str:store) ok += str, ok += "/";
+        // if(ok.size() > 1) ok.pop_back();
+
+        // return ok;
+
         string ok;
-        vector<string>store;
+        vector<string> store;
 
-        int i = 1, n = path.size();
+        int i = 1;
+        int n = path.size();
 
-        while(i < n) {
-            while(path[i]=='/') i++;
-            while(path[i] and path[i] != '/') ok += path[i++];
-            if(ok == ".." and store.size()) store.pop_back();
-            else if(ok.size() and ok != "." and ok != "..")
-            store.push_back(ok);
-                ok.clear();
+        while (i < n) {
+            while (i < n && path[i] == '/')
+                i++;
+            
+            while (i < n && path[i] != '/')
+                ok += path[i++];
+
+            if (ok == ".." && !store.empty()) {
+                store.pop_back();
+            }
+
+            else if (!ok.empty() && ok != "." && ok != "..") {
+                store.push_back(ok);
+            }
+
+            ok.clear();
         }
+
         ok = "/";
-        for(auto str:store) ok += str, ok += "/";
-        if(ok.size() > 1) ok.pop_back();
+
+        for (auto str : store) {
+            ok += str;
+            ok += "/";
+        }
+
+        if (ok.size() > 1)
+            ok.pop_back();
 
         return ok;
         
